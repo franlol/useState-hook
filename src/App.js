d450ef8cd27ae9a2493d9c0bce00d3f95ed09772
franlol <fraaaaan@gmail.com>
2019-05-27 17:44:17 +0200

    Testing useState Hook.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
+import InputField from './components/InputField/InputField';
 
 const App = () => {
-  const [state, setState] = useState({ count: 0 });
+  const [state, setState] = useState({
+    count: 0,
+    nameInput: '',
+    surnameInput: '',
+    pwdInput: '',
+  });
 
-  console.log(state)
+  const inputNameHandler = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  const update = () => {
+    setState({
+      ...state,
+      surnameInput: "Surname updated"
+    });
+  }
 
   return (
     <div className="App">
-      <p>count: {state.count}</p>
-      <button onClick={() => setState({ count: state.count + 1 })}>Add</button>
+      <p>App component: {state.count}</p>
+      <button onClick={() => setState({ ...state, count: state.count + 1 })}>Add</button>
+
+      <hr />
+
+      <InputField value={state.nameInput} name={"nameInput"} type={"text"} handler={inputNameHandler}>Name: </InputField>
+      <InputField value={state.surnameInput} name={"surnameInput"} type={"text"} handler={inputNameHandler}>Surname: </InputField>
+      <InputField value={state.pwdInput} name={"pwdInput"} type={"password"} handler={inputNameHandler}>Password: </InputField>
+
+      <button onClick={() => console.log(state)}>Check</button>
+      <button onClick={update}>Update</button>
     </div>
   );
 }
---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InputField = props => {
+    const { handler, value, name, type, children } = props;
+
+    return (
+        <>
+            <p>{children}<input name={name} type={type} value={value} onChange={(e) => handler(e)} /></p>
+        </>
+    );
+};
+
+export default InputField;
