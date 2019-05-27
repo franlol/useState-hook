658fea00a78b407d979ab6eb9dca2a1727c55e58
franlol <fraaaaan@gmail.com>
2019-05-27 17:45:51 +0200

    Fixing indentation

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const InputField = props => {
-    const { handler, value, name, type, children } = props;
+  const { handler, value, name, type, children } = props;
 
-    return (
-        <>
-            <p>{children}<input name={name} type={type} value={value} onChange={(e) => handler(e)} /></p>
-        </>
-    );
+  return (
+    <>
+      <p>{children}<input name={name} type={type} value={value} onChange={(e) => handler(e)} /></p>
+    </>
+  );
 };
 
 export default InputField;
